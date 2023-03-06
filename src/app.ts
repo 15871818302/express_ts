29975530f448887f8ad9529cc801e96f3c0cdb32
xiongshengzhi <1220718650@qq.com>
2023-03-06 20:23:31 +0800

    description: 初始化项目

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
 import express from "express";
+import routes from "./routes";
+
+// 创建 express 实例
+const app = express();
+
+// 全局挂载 json 解析中间件
+app.use(express.json());
+
+const PORT = 3000;
+
+// 启动服务
+app.listen(PORT, async () => {
+  console.log(`http://localhost:${PORT}`);
+  routes(app);
+});
